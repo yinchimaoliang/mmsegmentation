f311ea5fa1b564c72dd0f220951dac96f9154a5c
liyinhao <liyinhao@sensetime.com>
2020-11-27 18:15:42 +0800

    Change lr.

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_1024x1024_40k_pathology_full.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_1024x1024_40k_pathology_full.py
@@ -7,4 +7,6 @@ _base_ = [
 model = dict(
     decode_head=dict(num_classes=5), auxiliary_head=dict(num_classes=5))
 
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
+
 evaluation = dict(interval=500, metric='mDice')
