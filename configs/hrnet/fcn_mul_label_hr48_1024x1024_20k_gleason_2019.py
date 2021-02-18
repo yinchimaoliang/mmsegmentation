61ab5ff49a3cda87de38afcc7815b08ed00dd819
liyinhao <liyinhao0413@qq.com>
2021-02-18 13:44:06 +0800

    Change mul label to use 134

--- configs/hrnet/fcn_mul_label_hr48_1024x1024_20k_gleason_2019.py
+++ configs/hrnet/fcn_mul_label_hr48_1024x1024_20k_gleason_2019.py
@@ -31,13 +31,7 @@ model = dict(
         final_label_ind=0,
         pretrained='torchvision://resnet18',
         num_classes=4,
-        norm_cfg=norm_cfg,
-        # loss_decode=dict(
-        #     type='FocalLoss',
-        #     gauss_scale=5,
-        #     gauss_kernel=5,
-        #     gauss_sigma=3,
-        # )
+        norm_cfg=norm_cfg
     )
 )
 
@@ -45,7 +39,7 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
-        ann_dir=['train/3-labels/annotations', 'train/3-labels/Maps3_T', 'train/3-labels/Maps4_T', 'train/3-labels/Maps5_T']))
+        ann_dir=['train/3-labels/annotations', 'train/3-labels-134/Maps1_T', 'train/3-labels-134/Maps3_T', 'train/3-labels-134/Maps4_T']))
 
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 runner = dict(type='IterBasedRunner', max_iters=10000)
