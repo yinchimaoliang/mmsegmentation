a4e7289ef680b1279a70d88a87bd590c30ed4d96
liyinhao <liyinhao0413@qq.com>
2021-02-21 17:42:06 +0800

    increase lr for focal_loss_gauss_class_balance config

--- configs/hrnet/fcn_mul_label_hr48_1024x1024_20k_gleason_2019_focal_loss_gauss_class_balance.py
+++ configs/hrnet/fcn_mul_label_hr48_1024x1024_20k_gleason_2019_focal_loss_gauss_class_balance.py
@@ -79,8 +79,8 @@ data = dict(
     )
 )
 
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-5, by_epoch=False)
+optimizer = dict(type='SGD', lr=0.05, momentum=0.9, weight_decay=0.0005)
+lr_config = dict(policy='poly', power=0.9, min_lr=5e-4, by_epoch=False)
 runner = dict(type='IterBasedRunner', max_iters=10000)
 checkpoint_config = dict(by_epoch=False, interval=10000)
 evaluation = dict(interval=200, metric='mDice')
