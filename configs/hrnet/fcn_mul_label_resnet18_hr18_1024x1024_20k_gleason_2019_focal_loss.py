b20933b5285e398ea2aef1ed9d202ebe43636745
liyinhao <liyinhao0413@qq.com>
2021-02-26 14:59:48 +0800

    Add hrnet mul_label config

---
+++ configs/hrnet/fcn_mul_label_resnet18_hr18_1024x1024_20k_gleason_2019_focal_loss.py
@@ -0,0 +1,43 @@
+_base_ = './fcn_hr18_1024x1024_20k_gleason_2019_focal_loss.py'
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    decode_head=dict(
+        type='FCNMulLabelHead',
+        in_channels=[18, 36, 72, 144],
+        channels=sum([18, 36, 72, 144]),
+        wei_net_backbone=dict(
+            type='ResNet',
+            in_channels=3,
+            strides=(1, 1, 1, 1),
+            depth=18,
+            num_stages=4,
+            out_indices=(3,),
+            style='pytorch'),
+        wei_net_conv=dict(
+            type='Conv2d',
+            in_channels=512,
+            out_channels=3,
+            kernel_size=3,
+            padding=1
+        ),
+        mul_label_ind=[1, 2, 3],
+        final_label_ind=0,
+        pretrained='torchvision://resnet18',
+        num_classes=4,
+        norm_cfg=norm_cfg,
+        loss_decode=dict(
+            type='FocalLoss'
+        ),
+        loss_single=dict(
+             type='FocalLoss'
+        ),
+        sigma=1,
+        loss_step=1000
+    )
+)
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        ann_dir=['train/train/annotations', 'train/train/Maps1_T', 'train/train/Maps3_T', 'train/train/Maps4_T']))
